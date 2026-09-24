2c25fee416b1201c5e994fdaa606956fd1709f49
HamilRios7 <rioshamil7@gmail.com>
2026-09-24 12:53:36 +0200

    Add files via upload

---
+++ Practica1PSP/fork2.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void main() {
+  pid_t pid, pid_hijo, pid_padre, pid_propio;
+  
+
+  pid = fork();
+
+  
+  if (pid == 0 )  //Nos encontramos en Proceso hijo 
+  {        
+    printf("Hamil \n");	
+    
+      pid_hijo = getpid();
+     
+    
+    
+  }
+  else    //Nos encontramos en Proceso padre 
+  { 
+   pid_hijo = wait(NULL); //espera la finalización del proceso hijo
+   
+   
+        pid_propio = getpid();
+   printf("Mi PID es:%d\n" , pid_propio);
+   printf("Mi hijo tenia el PID:%d\n" , pid_hijo);
+  
+          
+   
+       
+  }
+   exit(0);
+}
