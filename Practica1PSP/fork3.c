174e2f506afdd2133e59ed088dcc542bf3451295
HamilRios7 <rioshamil7@gmail.com>
2026-09-24 13:04:07 +0200

    Ejercicio fork3.c
    Clarified process execution details and output order for fork behavior.

--- Practica1PSP/fork3.c
+++ Practica1PSP/fork3.c
@@ -14,11 +14,14 @@ a) ¿Cuántos procesos existen?
 Existen 2 procesos
 
 b) ¿ qué instrucciones ejecuta cada uno?
+El printf de Inicio solo lo ejecutara el padre.
+Ambos ejecutaran el printf "Después del fork", y luego dependiendo de que procesa sea, 
+uno hara printf de "Soy el hijo" si es el proceso hijo y " Soy el  padre" si es el proceso padre.
+ Despues el printf de Fin sera escrito por ambos procesos
 
-Ambos ejecutaran el printf después del fork, y luego dependiendo de que procesa sea, uno hara printf de hijo o de padre
 c) ¿cuántas veces aparece cada mensaje y qué partes de la salida pueden aparecer en distinto
 orden?
 El printf de inicio solo aparecera una vez por el padre
 El printf despues del fork, saldrá dos veces por padre e hijo
-El hijo printf solo aparecerá una vez
-El padre printf solo aparecerá una vez
+Los printf de soy el padre y soy el hijo apareceran en un orden aleatorio  cada vez que se ejecute el codigo
+EL print de fin aparecera dos veces , por padre e hijo
